eecce7e67f14d3046e60829839602919eb796f8e
Logesh Kumar R <logeshkumar.rajkumar@freshworks.com>
2020-04-12 16:57:41 +0530

    add covid endpoint

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cronTask = require("./cron");
 const connectDb = require("./src/connection");
 const User = require("./src/User.model");
 const Covid = require("./src/Covid.model");
-
+app.use(cors());
 const PORT = 5000;
 
 app.get("/users", async (req, res) => {
@@ -24,7 +25,14 @@ app.get("/user-create", async (req, res) => {
 app.get("/cron", async (req, res) => {
   await Covid.remove({});
   await cronTask.insertData();
-  res.send("Date imported \n");
+  res.send("Data imported \n");
+});
+
+app.get("/covid/:country", async (req, res) => {
+  const response = await Covid.find({
+    countryName: req.params.country,
+  });
+  res.status(200).json(response);
 });
 
 app.listen(PORT, function () {

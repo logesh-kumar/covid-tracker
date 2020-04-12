f279e9b657fd6f0cf4f1f5f349f358a1340daf1c
Logesh Kumar R <logeshkumar.rajkumar@freshworks.com>
2020-04-12 14:00:40 +0530

    change db name

--- src/connection.js
+++ src/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const User = require("./User.model");
 
-const connection = "mongodb://mongo:27017/mongo-test";
+const connection = "mongodb://mongo:27017/covid-tracker";
 
 const connectDb = () => {
   return mongoose.connect(connection);
